26adbd7991ac3b90834abc7a9e53195591837e68
Marc Nuri <marc@marcnuri.com>
2023-07-27 15:04:47 +0200

    feat(kwasmer): support for multiple controller deployment types

--- kwasmer/src/main/java/com/marcnuri/kwasmer/Deploy.java
+++ kwasmer/src/main/java/com/marcnuri/kwasmer/Deploy.java
@@ -1,16 +1,21 @@
 package com.marcnuri.kwasmer;
 
 
+import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
-import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.PodBuilder;
 import io.fabric8.kubernetes.api.model.PodSpecBuilder;
+import io.fabric8.kubernetes.api.model.PodTemplateSpecBuilder;
+import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder;
+import io.fabric8.kubernetes.api.model.batch.v1.JobBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import io.fabric8.kubernetes.client.utils.KubernetesSerialization;
+import io.quarkus.runtime.util.StringUtil;
 import jakarta.inject.Inject;
 import picocli.CommandLine;
 
+import java.util.Locale;
 import java.util.concurrent.Callable;
 
 import static com.marcnuri.kwasmer.Kwasmer.RUNTIME_CLASS_NAME;
@@ -22,9 +27,12 @@ public class Deploy implements Callable<Integer> {
   ContextMixin context;
   @Inject
   KubernetesSerialization kubernetesSerialization;
-
   @CommandLine.Parameters(index = "0", description = "The image to deploy (for example marcnuri/hello-world-of-wasm:js)")
-  private String image;
+  String image;
+  @CommandLine.Option(names = {"-n", "--name"}, description = "The name to use for the deployed controller (Pod, Job, etc.)")
+  String name;
+  @CommandLine.Option(names = {"-c", "--controller"}, description = "The controller to use for the deployment (one of: Pod, Job, Deployment)")
+  String controller;
 
   @Override
   public Integer call() {
@@ -32,23 +40,46 @@ public class Deploy implements Callable<Integer> {
     try (final KubernetesClient kc = new KubernetesClientBuilder()
       .withKubernetesSerialization(kubernetesSerialization)
       .build()) {
-      final String name = image.replaceAll("[^a-zA-Z0-9]", "-");
-      final Pod pod = new PodBuilder()
+      final String finalName = StringUtil.isNullOrEmpty(name) ?
+        image.replaceAll("[^a-zA-Z0-9]", "-") :
+        name;
+      final PodTemplateSpecBuilder podTemplate = new PodTemplateSpecBuilder()
         .withMetadata(new ObjectMetaBuilder()
-          .withName(name)
+          .withName(finalName)
           .addToAnnotations("module.wasm.image/variant", "compat-smart")
+          .addToLabels("app.kubernetes.io/name", finalName)
           .build())
         .withSpec(new PodSpecBuilder()
           .addNewContainer()
-          .withName(name)
+          .withName(finalName)
           .withImage(image)
           .endContainer()
           .withRuntimeClassName(RUNTIME_CLASS_NAME)
-          .build())
-        .build();
-      kc.pods().resource(pod).serverSideApply();
+          .build());
+      kc.resource(initController(podTemplate)).serverSideApply();
     }
     context.ansiOverwrite("✅  " + image + " deployed\n");
     return null;
+  }
+
+  private HasMetadata initController(PodTemplateSpecBuilder podTemplateSpec) {
+    return switch (controller.toLowerCase(Locale.ROOT)) {
+      case "deployment" -> new DeploymentBuilder()
+        .withMetadata(podTemplateSpec.buildMetadata())
+        .withNewSpec()
+        .withNewSelector().addToMatchLabels(podTemplateSpec.buildMetadata().getLabels()).endSelector()
+        .withTemplate(podTemplateSpec.build()).endSpec()
+        .build();
+      case "job" -> new JobBuilder()
+        .withMetadata(podTemplateSpec.buildMetadata())
+        .withNewSpec().withTemplate(podTemplateSpec
+          .editSpec().withRestartPolicy("Never").endSpec()
+          .build()).endSpec()
+        .build();
+      default -> new PodBuilder()
+        .withMetadata(podTemplateSpec.getMetadata())
+        .withSpec(podTemplateSpec.getSpec())
+        .build();
+    };
   }
 }
